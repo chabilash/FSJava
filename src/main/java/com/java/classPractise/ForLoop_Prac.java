13dfcc4a0c75b75afdc3d5b666ed53fc21636494
Abilash <chabilash@gmail.com>
2024-04-01 11:30:39 -0400

    Adding For Loop

---
+++ src/main/java/com/java/classPractise/ForLoop_Prac.java
@@ -0,0 +1,14 @@
+package com.java.classPractise;
+
+public class ForLoop_Prac {
+    public static void main(String[] args) {
+
+
+        for(int i=0;i<5;i++){
+            for(int j=4;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
